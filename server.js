f436244f93ba1629532314b960d836b27e445371
Aniket72094087100 <aniket72094087100@gmail.com>
2026-06-24 01:51:52 +0530

    Fix Jenkinsfile for Docker build

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 
 app.listen("3000", () => {
     
-    console.log(`server running at ${"http://localhost:3000"}`);
+    console.log(`server running at ${ "http://localhost:3000"}`);
 });
